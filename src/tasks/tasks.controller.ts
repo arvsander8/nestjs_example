9c0fbd8764bec014893d7f01707b0a9c03d47558
Alexander <arv.sander9@gmail.com>
2019-08-14 21:47:26 -0500

    Added tasks Controller by console commands

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,13 @@
+import {Controller, Get} from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {
+    
+    
+    @Get()
+    getTasks(){
+        return 'Retornando Tareas';
+    }
+    
+    
+}
