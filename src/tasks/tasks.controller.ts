e03613322b862ac9f9380da354d88f5af4e4b83a
Alexander <arv.sander9@gmail.com>
2019-08-14 23:18:07 -0500

    Added DTO to work with responses and requests

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import { CreateTaskDto } from "./dto/create-task.dto";
 
 @Controller('tasks')
 export class TasksController {
@@ -10,9 +11,22 @@ export class TasksController {
     }
 
     @Post()
-    createTask(@Body() task){
-        console.log(task);
+    createTask(@Body() task:CreateTaskDto): string{
+        console.log(task.description);
         return 'Creating a task';
+    }
+
+    @Delete(':id')
+    deleteTask(@Param('id') id): string {
+        console.log(id);
+        return `Deleting a task number: ${id}`;
+    }
+
+    @Put(':id')
+    updateTask(@Body() task:CreateTaskDto, @Param('id') id): string{
+        console.log(task);
+        console.log(id);
+        return 'Updating a task';
     }
     
 }
